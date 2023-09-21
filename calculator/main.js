a04add12fc4ad222a89ac4ad6c8ab3cc56989d89
ringmaker <chrisringmaker@gmail.com>
2023-09-20 23:50:27 -0400

    calculator button data tags

--- calculator/main.js
+++ calculator/main.js
@@ -1,8 +1,18 @@
-function operate(num1, op, num2) {
+const clearButton = document.getElementById('#clearBtn')
+const deleteButton = document.getElementById('#deleteBtn')
+const decimalButton = document.getElementById('#decimalBtn')
+const equalsButton = document.getElementById('#equalsBtn')
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const previousInput = document.getElementById('#previousInput')
+const currentInput = document.getElementById('#currentInput')
+
+function operate(num1, num2, op) {
   switch (op) {
-    case '+': console.log(num1 + num2); break;
-    case '-': console.log(num1 - num2); break;
-    case '*': console.log(num1 * num2); break;
-    case '/': console.log(num1 / num2); break;
+    case '+': num1 + num2;  break;
+    case '-': num1 - num2;  break;
+    case '*': num1 * num2;  break;
+    case '/': num1 / num2;  break;
+    case '^': num1 ** num2; break
   }
 }
