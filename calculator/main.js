ae00c3adc23a1ce1ed847280e12d0ba8e7a4914c
ringmaker <chrisringmaker@gmail.com>
2023-09-25 22:17:36 -0400

    basic functionality and screen display now working

--- calculator/main.js
+++ calculator/main.js
@@ -1,18 +1,62 @@
-const clearButton = document.getElementById('#clearBtn')
-const deleteButton = document.getElementById('#deleteBtn')
-const decimalButton = document.getElementById('#decimalBtn')
-const equalsButton = document.getElementById('#equalsBtn')
+const clearButton = document.getElementById('clearBtn')
+const deleteButton = document.getElementById('deleteBtn')
+const decimalButton = document.getElementById('decimalBtn')
+const equalsButton = document.getElementById('equalsBtn')
 const numberButtons = document.querySelectorAll('[data-number]')
 const operatorButtons = document.querySelectorAll('[data-operator]')
-const previousInput = document.getElementById('#previousInput')
-const currentInput = document.getElementById('#currentInput')
+const upperDisplay = document.getElementById('upperDisplay')
+const lowerDisplay = document.getElementById('lowerDisplay')
+
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+
+numberButtons.forEach((button) =>{
+  button.addEventListener('click', ()=> setCurrentInput(button.textContent))
+})
+
+operatorButtons.forEach((button) =>
+  button.addEventListener('click', ()=> setCurrentOperation(button.textContent) )
+)
+
+equalsButton.addEventListener('click', ()=> equals())
+clearButton.addEventListener('click', ()=> clearAll())
+
+function setCurrentInput(number) {
+  if(lowerDisplay.textContent === '0') {
+    lowerDisplay.textContent = ''
+}
+  lowerDisplay.textContent += number
+}
+
+function setCurrentOperation(operator) {
+  currentOperation = operator
+  firstOperand = lowerDisplay.textContent
+  upperDisplay.textContent = `${lowerDisplay.textContent} ${operator}`
+  lowerDisplay.textContent = ''
+}
+
+function equals() {
+  upperDisplay.textContent = `${upperDisplay.textContent} ${lowerDisplay.textContent} =`
+  secondOperand = lowerDisplay.textContent
+  lowerDisplay.textContent = operate(firstOperand, secondOperand, currentOperation)
+}
 
 function operate(num1, num2, op) {
+  num1 = Number(num1)
+  num2 = Number(num2)
+
   switch (op) {
-    case '+': num1 + num2;  break;
-    case '-': num1 - num2;  break;
-    case '*': num1 * num2;  break;
-    case '/': num1 / num2;  break;
-    case '^': num1 ** num2; break
+    case '+': return num1 + num2;
+    case '-': return num1 - num2;
+    case 'x': return num1 * num2;
+    case '÷': return num1 / num2;
+    case '^': return num1 ** num2;
   }
+}
+
+function clearAll() {
+  lowerDisplay.textContent = ''
+  upperDisplay.textContent = ''
+  currentOperation = null
 }
