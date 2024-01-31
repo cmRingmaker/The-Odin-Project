fdee3ae5772a11e84f070757a5df65c9a91ac7f4
ringmaker <chrisringmaker@gmail.com>
2024-01-30 21:15:11 -0500

    responsiveness with queries and grids

--- JavaScript/library/main.js
+++ JavaScript/library/main.js
@@ -1,1 +1,9 @@
 
+class Book {
+  constructor(title = 'Unknown', author = 'Unknown', pages = '0', isRead = false) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.isRead = isRead
+  }
+}
