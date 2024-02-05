a9997929fb6512a1b0149442e7f3159e39ee925f
ringmaker <chrisringmaker@gmail.com>
2024-02-05 00:43:40 -0500

    style updates, modal added

--- JavaScript/library/main.js
+++ JavaScript/library/main.js
@@ -1,3 +1,16 @@
+const template = document.getElementById('template')
+const container = document.getElementById('container')
+const editBooks = document.getElementById('editBooks')
+const addBooks = document.getElementById('addBooks')
+const modal = document.getElementById('modal')
+const closeModal = document.getElementById('closeModal')
+
+let library
+let content = template.content
+
+// -----------------------|
+// -------------- CLASSES |
+// -----------------------|
 
 class Book {
   constructor(title = 'Unknown', author = 'Unknown', pages = '0', isRead = false) {
@@ -6,4 +19,34 @@ class Book {
     this.pages = pages
     this.isRead = isRead
   }
+}
+
+// -----------------------|
+// ------ EVENT LISTENERS |
+// -----------------------|
+
+// MODAL POPUP
+addBooks.addEventListener('click', (e) => modal.showModal())
+closeModal.addEventListener('click', (e) => modal.close())
+
+// HANDLE EDITING SELECTED BOOK
+editBooks.addEventListener('click', (e) => console.log('dummy data'))
+
+
+// -----------------------|
+// ------------ FUNCTIONS |
+// -----------------------|
+
+
+function addNewBook() {
+  const newBook = new Book(title.value, author.value, pages.value, isRead.value)
+  library.push(newBook)
+}
+
+function updateLocalStorage() {
+  localStorage.setItem('library', JSON.stringify(library))
+}
+
+function checkLocalStorage() {
+  localStorage.getItem(library = JSON.parse(localStorage.getItem('library')))
 }
