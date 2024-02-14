cd10c95278117205fe9bac45286969ef09862e39
ringmaker <chrisringmaker@gmail.com>
2024-02-13 23:04:59 -0500

    content now renders when adding new books

--- JavaScript/library/main.js
+++ JavaScript/library/main.js
@@ -7,9 +7,10 @@ const closeModal = document.getElementById('closeModal')
 const bookPages = document.getElementById('bookPages')
 const myForm = document.getElementById('myForm')
 const submitForm = document.getElementById('submitForm')
+const formError = document.getElementById('formError')
 
-let library = []
 let content = template.content
+let library = JSON.parse(localStorage.getItem('library')) || [] // initialize localstorage library
 
 // -----------------------|
 // -------------- CLASSES |
@@ -30,7 +31,10 @@ class Book {
 
 // MODAL POPUP
 addBooks.addEventListener('click', (e) => modal.showModal())
-closeModal.addEventListener('click', (e) => modal.close())
+closeModal.addEventListener('click', (e) => {
+  errorMessage() // handle error div
+  modal.close()
+})
 
 bookPages.addEventListener('keydown', (e) => { // force numbers only
   if(/[0-9\-.]|Delete|Backspace/.test(e.key)) return
@@ -39,6 +43,7 @@ bookPages.addEventListener('keydown', (e) => { // force numbers only
 
 submitForm.addEventListener('click', (e) => {
   e.preventDefault()
+  errorMessage() // handle error div
 
   const title = myForm.bookName.value
   const author = myForm.bookAuthor.value
@@ -46,12 +51,9 @@ submitForm.addEventListener('click', (e) => {
   const isRead = myForm.readOrNot.value === 'yes'
   
   const newBook = new Book(title, author, pages, isRead)
-  console.log(newBook)
-  // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
-  // put data into library - and don't allow a form submit without all values filled out!
+
+  addNewBook(newBook)
 })
-
-
 
 // HANDLE EDITING SELECTED BOOK
 editBooks.addEventListener('click', (e) => console.log('dummy data'))
@@ -60,19 +62,63 @@ editBooks.addEventListener('click', (e) => console.log('dummy data'))
 // ------------ FUNCTIONS |
 // -----------------------|
 
-function render() {
-  console.log('rendering')
+function renderPageLoad() {
+  library.forEach(book => {    
+    let div = template.content.cloneNode(true)
+
+    div.querySelector('#cardTitle').textContent = book.title
+    div.querySelector('#cardAuthor').textContent = book.author
+    div.querySelector('#cardPages').textContent = book.page
+    div.querySelector('#cardRead').textContent = book.isRead
+
+    container.append(div)
+  })
 }
 
-function addNewBook() {
-  const newBook = new Book(title, author, pages, isRead)
-  library.push(newBook)
+function renderLatestBook() {
+  checkLocalStorage()
+  let latest = library.slice(-1)
+
+  let div = template.content.cloneNode(true)
+
+  div.querySelector('#cardTitle').textContent = latest[0].title
+  div.querySelector('#cardAuthor').textContent = latest[0].author
+  div.querySelector('#cardPages').textContent = latest[0].page
+  div.querySelector('#cardRead').textContent = latest[0].isRead
+
+  container.append(div)
 }
 
+function errorMessage() {
+  formError.style.visibility = 'hidden'
+}
 function updateLocalStorage() {
   localStorage.setItem('library', JSON.stringify(library))
 }
 
 function checkLocalStorage() {
-  localStorage.getItem(library = JSON.parse(localStorage.getItem('library')))
+  localStorage.getItem(JSON.parse(localStorage.getItem('library')))
 }
+
+function addNewBook(book) {
+  // checkLocalStorage()
+
+  let duplicate = library.find(b => 
+    b.title === book.title &&
+    b.author === book.author &&
+    b.pages === book.pages &&
+    b.isRead === book.isRead
+  )
+
+  if(!duplicate) {
+    library.push(book)
+    renderLatestBook()
+    modal.close()
+  } else {
+    formError.style.visibility = 'visible'
+  }
+
+  updateLocalStorage()
+}
+
+renderPageLoad()
