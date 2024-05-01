6a50c3f62b7375adb3d84883d8182adab596809c
ringmaker <chrisringmaker@gmail.com>
2024-05-01 00:26:13 -0400

    menu data arrays are now correctly working

--- Full Stack JavaScript/JavaScript/restaurant-page/src/menu.js
+++ Full Stack JavaScript/JavaScript/restaurant-page/src/menu.js
@@ -5,16 +5,24 @@ import Fries from './img/fries.png'
 import Shake from './img/shake.png'
 
 const menuItems = [
+  // Create images from DOMUtils
   {src: Burger, name: 'Hamburger'},
   {src: Fries, name: 'Fries'},
   {src: Shake, name: 'Chocolate Shake'}
+]
+
+const menuData = [
+  // Create Data from DOMUtils
+  {tagName: 'span', content: `Fake Review 1 \n -Review Man`, className: 'about-review'},
+  {tagName: 'span', content: `Fake Review 2 \n -Review Woman`, className: 'about-review'},
+  {tagName: 'span', content: `Fake Review 3 \n -Review Boy`, className: 'about-review'},
 ]
 
 export default function menu() {
   console.log('Menu.js working')
   const container = DOMUtils.createDiv('menu-container')
   
-  menuItems.forEach((item) => {
+  menuItems.forEach((item, index) => {
     const menuItem = DOMUtils.createDiv('menu-item')
     const itemInfo = DOMUtils.createDiv('menu-info')
     const imgContainer = DOMUtils.createDiv('img-container')
@@ -24,9 +32,12 @@ export default function menu() {
     menuItem.append(itemInfo, imgContainer)
     imgContainer.appendChild(menuImg)
 
-    // Test text for info in menu section - will move to its own loop
-    const review1 = DOMUtils.createElementWithContent('span', `Fake Review 1 \n -Review Man`, 'about-review')
-    itemInfo.appendChild(review1)
+    // Test text for info in menu section ---
+    if(menuData[index]) {
+      const review = DOMUtils.createElementWithContent(menuData[index].tagName, menuData[index].content, menuData[index].className)
+      itemInfo.appendChild(review)
+    }
+
   })
 
 
