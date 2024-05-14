827502265d252a6813561603c590ba4a4957a692
ringmaker <chrisringmaker@gmail.com>
2024-05-14 00:34:49 -0400

    nav css and event listeners

--- Full Stack JavaScript/JavaScript/todo-list/src/js/index.js
+++ Full Stack JavaScript/JavaScript/todo-list/src/js/index.js
@@ -14,10 +14,25 @@ import TODO from './TODOS'
 // -------------------------
 // DOM ELEMENTS
 
-const taskList = document.getElementById('taskList')
+const taskList =    document.getElementById('task-list')
+const menuTask =    document.getElementById('menu-tasks').querySelectorAll('li')
+const menuPrio =    document.getElementById('menu-priority').querySelectorAll('li')
+const menuProject = document.getElementById('menu-project').querySelectorAll('li')
 
 // -------------------------
 // EVENT LISTENERS
+
+menuTask.forEach((group) => {
+  group.addEventListener('click', () => activeGroup('tasks', group))
+})
+
+menuPrio.forEach((group) => {
+  group.addEventListener('click', () => activeGroup('priority', group))
+})
+
+menuProject.forEach((group) => {
+  group.addEventListener('click', () => activeGroup('project', group))
+})
 
 
 /* ==================================================================
@@ -37,4 +52,22 @@ for(let i = 1; i < 35; i++) {
   const task = TODO.createTask(`task ${i}`)
   
   taskList.appendChild(task)
+}
+
+
+function activeGroup(groupName, targetElement) {
+  const groups = {
+    'tasks': menuTask,
+    'priority': menuPrio,
+    'project': menuProject
+  }
+
+  const groupElements = groups[groupName]
+
+  groupElements.forEach(element => {
+    element.classList.remove('active')
+    element
+  })
+
+  targetElement.classList.add('active')
 }
