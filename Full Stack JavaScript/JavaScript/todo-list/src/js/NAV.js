161fc2d2b279c0f3a0c5cb666ce30570fee1b6b6
ringmaker <chrisringmaker@gmail.com>
2024-05-15 19:55:01 -0400

    seperate nav js handlers into their own file

--- Full Stack JavaScript/JavaScript/todo-list/src/js/NAV.js
+++ Full Stack JavaScript/JavaScript/todo-list/src/js/NAV.js
@@ -1,3 +1,53 @@
 export default class NAV {
-// code here
+  constructor() {
+    this.sidebar = document.getElementById('sidebar')
+    this.menuTask = document.getElementById('menu-tasks').querySelectorAll('li')
+    this.menuPrio = document.getElementById('menu-priority').querySelectorAll('li')
+    this.menuProject = document.getElementById('menu-project').querySelectorAll('li')
+    this.sidebarToggle = document.getElementById('sidebar-toggle')
+
+    this.initEventListeners()
+  }
+
+  initEventListeners() {
+    this.menuTask.forEach((group) => {
+      group.addEventListener('click', () => this.activeGroup('tasks', group))
+    })
+    
+    this.menuPrio.forEach((group) => {
+      group.addEventListener('click', () => this.activeGroup('priority', group))
+    })
+    
+    this.menuProject.forEach((group) => {
+      group.addEventListener('click', () => this.activeGroup('project', group))
+    })
+    
+    this.sidebarToggle.addEventListener('click', () => this.toggleSidebar())
+  }
+
+  activeGroup(groupName, targetElement) {
+    const groups = {
+      'tasks': this.menuTask,
+      'priority': this.menuPrio,
+      'project': this.menuProject
+    }
+  
+    const groupElements = groups[groupName]
+  
+    groupElements.forEach(element => {
+      element.classList.remove('active')
+    })
+  
+    targetElement.classList.add('active')
+  }
+  
+  toggleSidebar(){
+    if(this.sidebar.classList.contains('show-sidebar')) {
+      this.sidebar.classList.remove('show-sidebar')
+      this.sidebar.classList.add('hide-sidebar')
+    } else {
+      this.sidebar.classList.remove('hide-sidebar')
+      this.sidebar.classList.add('show-sidebar')
+    }
+  }
 }
--- Full Stack JavaScript/JavaScript/todo-list/src/js/index.js
+++ Full Stack JavaScript/JavaScript/todo-list/src/js/index.js
@@ -10,35 +10,15 @@
 import '../css/style.css'
 import '../css/reset.css'
 import TODO from './TODOS'
+import NAV from './NAV'
 
 // -------------------------
 // DOM ELEMENTS
 
 const taskList =    document.getElementById('task-list')
 
-const sidebar =     document.getElementById('sidebar')
-const menuTask =    document.getElementById('menu-tasks').querySelectorAll('li')
-const menuPrio =    document.getElementById('menu-priority').querySelectorAll('li')
-const menuProject = document.getElementById('menu-project').querySelectorAll('li')
-
-const sidebarToggle = document.getElementById('sidebar-toggle')
-
 // -------------------------
 // EVENT LISTENERS
-
-menuTask.forEach((group) => {
-  group.addEventListener('click', () => activeGroup('tasks', group))
-})
-
-menuPrio.forEach((group) => {
-  group.addEventListener('click', () => activeGroup('priority', group))
-})
-
-menuProject.forEach((group) => {
-  group.addEventListener('click', () => activeGroup('project', group))
-})
-
-sidebarToggle.addEventListener('click', () => toggleSidebar())
 
 
 /* ==================================================================
@@ -49,41 +29,10 @@ sidebarToggle.addEventListener('click', () => toggleSidebar())
     * description
    ================================================================== */
 
-
-// const task1 = TODO.createTask('mothafucka')
-
-// content.appendChild(task1)
+const nav = new NAV()
 
 for(let i = 1; i < 35; i++) {
   const task = TODO.createTask(`task ${i}`)
   
   taskList.appendChild(task)
 }
-
-function activeGroup(groupName, targetElement) {
-  const groups = {
-    'tasks': menuTask,
-    'priority': menuPrio,
-    'project': menuProject
-  }
-
-  const groupElements = groups[groupName]
-
-  groupElements.forEach(element => {
-    element.classList.remove('active')
-    element
-  })
-
-  targetElement.classList.add('active')
-}
-
-function toggleSidebar(){
-  if(sidebar.classList.contains('show-sidebar')) {
-    sidebar.classList.remove('show-sidebar')
-    sidebar.classList.add('hide-sidebar')
-  } else {
-    sidebar.classList.remove('hide-sidebar')
-    sidebar.classList.add('show-sidebar')
-  }
-}
-
