5dbf2fcb35c480b985dad5d73d830efad9bc4dc2
Ringmaker <chrisringmaker@gmail.com>
2025-08-12 23:22:16 -0400

    Add knights-travails project folder

---
+++ Full Stack JavaScript/JavaScript/knights-travails/main.js
@@ -0,0 +1,6 @@
+// TODO: Make the dang thang
+
+function knightMoves() {
+  // Function to show the shortest possible way to get from one square to another
+  // by outputting all squares the knight will stop on along the way.
+}
